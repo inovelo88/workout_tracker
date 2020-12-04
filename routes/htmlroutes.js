46b806fcd406023a4c7c0590783f53cd244955f3
Irene Novelo <iyn88@yahoo.com>
2020-12-03 16:17:39 -0800

    added routes

---
+++ routes/htmlroutes.js
@@ -0,0 +1,15 @@
+// Ask Namita if correct and needed
+
+const path = require("path");
+
+module.exports = function(app) {
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+};
